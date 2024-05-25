2fadac94d2f854476ec7f6c488a7637418acb14a
mperulinan <miguelzasder@yahoo.es>
2024-05-25 16:22:45 +0200

    Deleted the file 'AssetWithHoldings.cs'.

--- Portfolio.Server/Models/Others/AssetWithHoldings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Portfolio.Server.Models.Others
-{
-    public class AssetWithHoldings
-    {
-        public required string AssetId { get; set; }
-        public decimal Holdings { get; set; }
-    }
-}
